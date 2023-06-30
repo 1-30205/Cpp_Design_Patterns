29d0f3dfa7288129ccf70820b9e79e1daab857d1
wubo <1723521395@qq.com>
2023-07-01 01:38:31 +0800

    update .gitignore

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdint>
+#include <type_traits>
+#include <utility>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <array>
+#include <list>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <algorithm>
+#include <memory>
+#include <thread>
+#include <future>
+#include <atomic>
+#include <chrono>
+#include <cxxabi.h>
+
+int main(int argc, const char *argv[]) {
+    return 0;
+}
